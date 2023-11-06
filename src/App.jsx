929c5e495bb4ea21445b79fe1fdbe4d1a6654df0
claudiomr79 <30968556+claudiomr79@users.noreply.github.com>
2023-11-06 19:44:23 -0300

    second commit
    header, main, footer components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header name="Claudio Rohr" home="Home" about="About" proyects="Proyects" contact="Contact" />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
